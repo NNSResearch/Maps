71025bdf7699e2e03d3a27e5b8c2cc7d8d68103d
No Name Studio <109845220+NNSResearch@users.noreply.github.com>
2022-08-04 19:59:00 +0800

    Create 1.js

---
+++ js/1.js
@@ -0,0 +1,28 @@
+<html>
+<head>
+<script
+src="//maps.googleapis.com/maps/api/js?key=AIzaSyDJW4jsPlNKgv6jFm3B5Edp5ywgdqLWdmc&sensor=false">
+</script>
+
+<script>
+var myCenter=new google.maps.LatLng(45.434046,12.340284);
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:18,
+  mapTypeId:google.maps.MapTypeId.HYBRID
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+</head>
+
+<body>
+<div id="googleMap" style="width:500px;height:380px;"></div>
+</body>
+</html>
